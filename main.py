5ff30590a38268f3ce94feff021936c12e4dd89e
Sugar <topevol33@gmail.com>
2024-08-23 23:11:30 +0300

    Making detection faster

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-from util import get_parking_spots_bboxes
+from util import get_parking_spots_bboxes, empty_or_not
 from networkx import connected_components
 
 mask_path = './data/mask_crop.png'
@@ -12,20 +12,39 @@ connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 
 spots = get_parking_spots_bboxes(connected_components)
 
+spots_status = [None for j in spots]
+
+frame_num = 0
 ret = True
+step = 30
 while ret:
     ret, frame = cap.read()
+    #updating stop_status only once in 30 frames
+    if frame_num % step == 0 :
+        for spot_indx, spot in enumerate(spots):
+            x1, y1, w, h = spot
 
-    for spot in spots:
-        x1, y1, w, h = spot
+            spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
 
-        frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (255, 0, 0))
+            spot_status = empty_or_not(spot_crop)
+
+            spots_status[spot_indx] = spot_status
+    #drawing with the last spot_status
+    for spot_indx, spot in enumerate(spots):
+        spot_status = spots_status[spot_indx]
+        x1, y1, w, h = spots[spot_indx]
+        if spot_status:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0))
+        else:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255))
 
     if frame is not None:
         cv2.imshow('frame', frame)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
+
+    frame_num += 1
 
 cap.release()
 cv2.destroyAllWindows()
